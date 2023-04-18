49a801d055598687bc29beac60f11c0dd63289a4
Alex Woersching <ajw006@gmail.com>
2023-04-17 22:23:01 -0700

    Creates basic concept code
    Uses the global variables to slowly increase the intensity of the LEDs
    at a specific time.

--- artificial_sun.ino
+++ artificial_sun.ino
@@ -4,62 +4,63 @@
 
 RTC_DS1307 RTC;
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Wire.begin();
-  int beg = RTC.begin();
-  if(beg){
-    Serial.println("RTC Begins...");
-  }
-  else{
-    Serial.println("RTC Hasn't Begun?");
-  }
-  /*
-  while (! RTC.isrunning()) {
-    Serial.println("RTC is NOT running!");
-    // following line sets the RTC to the date & time this sketch was compiled
-    delay(10);
-  }
-  */
-  /*
-  if(RTC.isrunning()){
-    Serial.println("RTC *is* Running!");
-  }
-  else{
-    Serial.println("RTC is not Running?");
-  }
-  */
-  RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
-//  Serial.println("Time set");
+const char ledPin = 3;
+unsigned char pinOut = 0;
 
-  pinMode(3, OUTPUT);
+// The below variables are arbitrary and 
+// should be determined by the user
+const uint32_t secondsInDay = uint32_t(60)*60*24;
+const char alarmHour = 22; // Wake up hour
+const char alarmMinute = 00; // Wake up minute
+const char alarmPeriod = 10; // Minutes to get to full intensity
+
+uint32_t now, alarmTime = (uint32_t(alarmHour)*60*60) + (alarmMinute*60);
+const uint32_t alarmTimePlusPeriod = alarmTime + (alarmPeriod*60);
+
+void setup() {
+    Serial.begin(9600);
+    Wire.begin();
+    int rtcBegin = RTC.begin();
+    if(rtcBegin){
+        Serial.println("RTC Begins...");
+    }
+    else{
+        Serial.println("RTC Hasn't Begun?");
+    }
+    /*
+    while (! RTC.isrunning()) {
+        Serial.println("RTC is NOT running!");
+        // following line sets the RTC to the date & time this sketch was compiled
+        delay(10);
+    }
+    */
+    /*
+    if(RTC.isrunning()){
+        Serial.println("RTC *is* Running!");
+    }
+    else{
+        Serial.println("RTC is not Running?");
+    }
+    */
+    RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    Serial.println("Time set");
+
+    pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  DateTime now = RTC.now();
+	now = RTC.now().unixtime() % secondsInDay;
 
-  uint8_t sec = now.second();
-
-  if(sec >= 0 && sec < 15){
-    Serial.println(0);
-    analogWrite(3, 0);
-  }
-  else if(sec >= 15 && sec < 30){
-    int output = map(sec, 15, 30, 0, 255);
-    Serial.println(output);
-    analogWrite(3, output);
-  }
-  else if(sec >= 30 && sec < 45){
-    Serial.println(255);
-    analogWrite(3, 255);
-  }
-  else{
-    int output = map(sec, 45, 60, 255, 0);
-    Serial.println(output);
-    analogWrite(3, output);
-  }
-
-  // Serial.println(now.timestamp());
-  delay(1000);
+    pinOut = map(now, alarmTime, alarmTimePlusPeriod, 0, 255);
+    if(now >= alarmTime && now < alarmTimePlusPeriod){
+        analogWrite(ledPin, pinOut);
+    }
+    else if(now >= alarmTimePlusPeriod){
+        analogWrite(ledPin, 255);
+    }
+    
+    Serial.println(RTC.now().timestamp());
+    Serial.println(pinOut);
+    
+	delay(1000);
 }
