b08b4c84effb640ae2f0c2e12dbb6eb4b81f705f
Alex Woersching <ajw006@gmail.com>
2023-04-24 22:53:48 -0700

    Checks that RTC is running
    Adds a debugging boolean to turn true or false depending on whether it's
    necessary to print timer info to the console.
    Changed the `loop()` to always `analogWrite()` to `ledPin` and instead
    modify the `pinOut` within the if statements. This makes debugging a
    little easier, as I can always print `pinOut` to determine the output of
    `ledPin`.
    Added some arbitrary `Serial.print()`'s that are wrapped in a debugging
    if-statement.
    Uncommented the `RTC.isrunning()` while-loop in `setup()`. This was
    originally commented out because of an issue where the RTC module would
    read as though it wasn't running, even though it was telling the time
    perfectly fine. After reviewing the DS1307 data sheet, the error
    occurred in supplying the RTC module with 3.3V instead of the
    recommended 5V. Once this swap was made, no issues have been noticed.

--- artificial_sun.ino
+++ artificial_sun.ino
@@ -1,47 +1,39 @@
-//#include <TimeLib.h>
 #include <Wire.h>
 #include <RTClib.h>
 
 RTC_DS1307 RTC;
 
+const bool debug = false; // Switch to true if printing to console is desirable
+
 const char ledPin = 3;
 unsigned char pinOut = 0;
 
-// The below variables are arbitrary and 
+// The below variables are arbitrary and
 // should be determined by the user
 const uint32_t secondsInDay = uint32_t(60)*60*24;
 const char alarmHour = 22; // Wake up hour
-const char alarmMinute = 00; // Wake up minute
-const char alarmPeriod = 10; // Minutes to get to full intensity
+const char alarmMinute = 50; // Wake up minute
+const char alarmPeriod = 3; // Minutes to get to full intensity
 
 uint32_t now, alarmTime = (uint32_t(alarmHour)*60*60) + (alarmMinute*60);
-const uint32_t alarmTimePlusPeriod = alarmTime + (alarmPeriod*60);
+uint32_t alarmTimePlusPeriod = alarmTime + (alarmPeriod*60);
 
 void setup() {
     Serial.begin(9600);
     Wire.begin();
-    int rtcBegin = RTC.begin();
-    if(rtcBegin){
+    if(RTC.begin()){
         Serial.println("RTC Begins...");
     }
     else{
         Serial.println("RTC Hasn't Begun?");
     }
-    /*
+
+    // Check if RTC module is running
     while (! RTC.isrunning()) {
         Serial.println("RTC is NOT running!");
-        // following line sets the RTC to the date & time this sketch was compiled
         delay(10);
     }
-    */
-    /*
-    if(RTC.isrunning()){
-        Serial.println("RTC *is* Running!");
-    }
-    else{
-        Serial.println("RTC is not Running?");
-    }
-    */
+
     RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
     Serial.println("Time set");
 
@@ -51,16 +43,25 @@ void setup() {
 void loop() {
 	now = RTC.now().unixtime() % secondsInDay;
 
-    pinOut = map(now, alarmTime, alarmTimePlusPeriod, 0, 255);
     if(now >= alarmTime && now < alarmTimePlusPeriod){
-        analogWrite(ledPin, pinOut);
+        pinOut = map(now, alarmTime, alarmTimePlusPeriod, 30, 255);
     }
     else if(now >= alarmTimePlusPeriod){
-        analogWrite(ledPin, 255);
+        pinOut = 255;
     }
+    analogWrite(ledPin, pinOut);
     
-    Serial.println(RTC.now().timestamp());
-    Serial.println(pinOut);
+    if(debug){
+        Serial.print(RTC.now().unixtime());
+        Serial.print(" - (");
+        Serial.print(RTC.now().timestamp());
+        Serial.println(")");
+        Serial.print(alarmTime);
+        Serial.print(" - ");
+        Serial.println(alarmTimePlusPeriod);
+        Serial.println(now);
+        Serial.println(pinOut);
+    }
     
 	delay(1000);
 }
