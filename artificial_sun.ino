8fcd8ef24bdd04ac791fe1ebf2d774e63ea722fc
Alex Woersching <ajw006@gmail.com>
2023-04-27 23:00:11 -0700

    Implements Timestamp class
    Creates a Timestamp class which keeps track of the hours and minutes of
    the day, as well as the seconds representation of the 24 hour day.
    This class is meant to represent the clock time of any day regardless of
    the date. It also doesn't keep track of seconds, since precision is not
    of utmost importance for the use case of this class.
    With this class, the code will be revamped to easily construct
    Timestamps for various representations of time, including the alarm
    time, the alarm time plus the period, and the future timeout time which
    will turn off the LED's at a certain point when there should *ideally*
    be enough natural light in the room provided by the irl sun.

--- artificial_sun.ino
+++ artificial_sun.ino
@@ -3,20 +3,71 @@
 
 RTC_DS1307 RTC;
 
-const bool debug = false; // Switch to true if printing to console is desirable
+const bool debug = true; // Switch to true if printing to console is desirable
 
 const char ledPin = 3;
 unsigned char pinOut = 0;
+const uint32_t secondsInDay = uint32_t(60)*60*24;
+const char alarmPeriod = 30; // Minutes to get to full intensity
 
 // The below variables are arbitrary and
 // should be determined by the user
-const uint32_t secondsInDay = uint32_t(60)*60*24;
-const char alarmHour = 22; // Wake up hour
-const char alarmMinute = 50; // Wake up minute
-const char alarmPeriod = 3; // Minutes to get to full intensity
+const char alarmHour = 07; // Wake up hour
+const char alarmMinute = 30; // Wake up minute
 
 uint32_t now, alarmTime = (uint32_t(alarmHour)*60*60) + (alarmMinute*60);
 uint32_t alarmTimePlusPeriod = alarmTime + (alarmPeriod*60);
+
+class Timestamp{
+    private:
+        char hour;
+        char minute;
+        uint32_t day_seconds; // Seconds in the day, should max out at 86399
+
+    public:
+        // Constructors
+        Timestamp(char hour){
+            this->hour = hour;
+            this->minute = 0;
+            this->day_seconds = (uint32_t(this->hour)*60*60) + (this->minute*60);
+        }
+        Timestamp(char hour, char minute){
+            this->hour = hour;
+            this->minute = minute;
+            this->day_seconds = (uint32_t(this->hour)*60*60) + (this->minute*60);
+        }
+        Timestamp(uint32_t seconds){
+            this->hour = char((seconds/60)/60);
+            this->minute = (seconds - (this->hour*60*60))/60;
+            this->day_seconds = seconds;
+        }
+        // Getters
+        char getHour(){
+            return this->hour;
+        }
+        char getMinute(){
+            return this->minute;
+        }
+        uint32_t getDaySeconds(){
+            return this->day_seconds;
+        }
+        // Operators
+        bool operator==(Timestamp right){
+            return day_seconds == right.getDaySeconds();
+        }
+        bool operator>(Timestamp right){
+            return day_seconds > right.getDaySeconds();
+        }
+        bool operator<(Timestamp right){
+            return day_seconds < right.getDaySeconds();
+        }
+        bool operator>=(Timestamp right){
+            return day_seconds >= right.getDaySeconds();
+        }
+        bool operator<=(Timestamp right){
+            return day_seconds <= right.getDaySeconds();
+        }
+};
 
 void setup() {
     Serial.begin(9600);
